b4a32b886e8ad39f5ce90566ed1aa7f2955fb047
Christopher Ryan <cmr119@yahoo.com>
2014-01-31 23:10:49 -0500

    Initial commit.  Ext JS fully wired up.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.0.2'
+gem 'pg', '0.17.1'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '~> 2.4.0'
+#gem 'coffee-rails', '~> 4.0.0'
+#gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails', '~> 3.0.4'
+gem 'turbolinks', '2.2.0'
+gem 'jbuilder', '~> 1.5.3'
+#gem 'devise', '~> 3.2.2'
+gem 'foreigner', '~> 1.6.1'
+gem 'mechanize', '~> 2.7.2'
+
+group :development, :test do
+  gem 'debugger', '1.6.5'
+  gem 'rspec-rails', '~> 2.14.1'
+end
+
+group :doc do
+  gem 'sdoc', '~> 0.4.0', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindASchoolInPa::Application.config.secret_key_base = '906bbce34210c0ff59c74a50b17e215586a8c720cd1437dcb146c535c1c6cf25dab78a76aac69d62239cbf023dd65bd23117c3e050bd0026038d1b10ad115cfd'
